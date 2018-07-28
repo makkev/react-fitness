ea353647682d79e45a990fdac53671303303f671
K Mak <kumarajiva08@gmail.com>
2018-07-28 19:19:17 +1000

    add entry text button

--- components/AddEntry.js
+++ components/AddEntry.js
@@ -4,6 +4,8 @@ import { getMetricMetaInfo, timeToString } from '../utils/helpers';
 import UdaciSlider from './UdaciSlider';
 import UdaciSteppers from './UdaciSteppers';
 import DateHeader from './DateHeader';
+import { Ionicons } from '@expo/vector-icons';
+import TextButton from './TextButton';
 
 function SubmitBtn ({ onPress }) {
   return (
@@ -73,12 +75,33 @@ export default class AddEntry extends Component {
     // Save to 'DB'
 
     // Clear local notification
+  }
 
+  reset = () => {
+    const key = timeToString()
 
+    // Update Redux
+    // Route to Home
+    // Update "DB"
   }
 
   render() {
     const metaInfo = getMetricMetaInfo();
+
+    if (this.props.alreadyLogged) {
+      return (
+        <View>
+          <Ionicons
+            name={'ios-happy-outline'}
+            size={100}
+          />
+          <Text>You already logged your information for today.</Text>
+          <TextButton onPress={this.reset}>
+            Reset
+          </TextButton>
+        </View>
+      );
+    }
 
     return (
       <View>
